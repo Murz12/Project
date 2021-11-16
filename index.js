fdbd57bbf84254fef3e1aa4b43bd620b8baaa216
example@gmail.com <example@gmail.com>
2021-11-16 16:54:57 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let person = {
+    name: 'Jhon',
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.name + "'s "  + 'age ' + 'is ' + person.age);
+
+let arr = ['plm', 'orange', 'apple.bmp'];
+console.log(arr[1]);
+
+let answer = prompt('Are you over 18', 'yes');
+console.log(typeof(person));
+
